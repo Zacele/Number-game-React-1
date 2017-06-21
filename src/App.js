81fa1e1d052029e6f3017d6ce1125dcfabd13368
Khoa Le\admin <le.khoa2209@gmail.com>
2017-06-21 12:34:54 +0700

    HTML skeletons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import Quiz from './Component/Quiz';
+
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Add Number game</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Quiz/>
         </p>
       </div>
     );
